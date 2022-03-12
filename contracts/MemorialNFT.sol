9ff2b189c86a06e30f4ad1413818e8d701705258
dorakemon <51844896+dorakemon@users.noreply.github.com>
2022-03-12 15:44:53 +0900

    add function getTokenIdBySender

--- contracts/MemorialNFT.sol
+++ contracts/MemorialNFT.sol
@@ -6,41 +6,54 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Burnable is ERC721URIStorage {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
 
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+  uint256 MINT_PRICE = 0.5 ether;
 
-    // mint価格をここで定義
-    // ここが最低価格になる。 0.01ETHではなく、1ETHでも可。
-    uint MINT_PRICE = 1 ether;
+  mapping(address => uint256) public ownerNFTCount;
 
-    constructor () ERC721 ("Burnable", "BURN") {}
+  constructor() ERC721("Burnable", "BURN") {}
 
-    // mint処理
-    function mintBurnable(string memory tokenURI) public payable returns (uint256) { 
-        // mint時に合わせて 0.01ether を送ってきているか確認
-        require(msg.value == MINT_PRICE);
+  function mintBurnable(string memory tokenURI)
+    public
+    payable
+    returns (uint256)
+  {
+    require(msg.value == MINT_PRICE);
 
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
+    _tokenIds.increment();
+    uint256 newItemId = _tokenIds.current();
 
-        // 実際のmint処理
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+    _mint(msg.sender, newItemId);
+    _setTokenURI(newItemId, tokenURI);
 
-        return newItemId;
+    ownerNFTCount[msg.sender]++;
+
+    return newItemId;
+  }
+
+  function burn(uint256 _tokenId) public {
+    require(ownerOf(_tokenId) == msg.sender);
+
+    // burn nft
+    _transfer(msg.sender, 0x000000000000000000000000000000000000dEaD, _tokenId);
+
+    // pay back
+    address payable receiver = payable(msg.sender);
+    receiver.transfer(MINT_PRICE);
+    ownerNFTCount[msg.sender]--;
+  }
+
+  function getTokenIdBySender() public view returns (uint256[] memory) {
+    uint256[] memory result = new uint256[](ownerNFTCount[msg.sender]);
+    uint256 counter = 0;
+    for (uint256 i = 1; i <= _tokenIds.current(); i++) {
+      if (ownerOf(i) == msg.sender) {
+        result[counter] = i;
+        counter++;
+      }
     }
-
-    // 換金burn処理
-    function burn(uint256 _tokenId) public { 
-        // そのNFT所有者かどうか確認
-        require(ownerOf(_tokenId) == msg.sender);
-
-        // そのNFTを無に転送する (burn)
-        _transfer(msg.sender, 0x000000000000000000000000000000000000dEaD, _tokenId);
-
-        // mint時に払った0.01etherを返す処理
-        address payable receiver = payable(msg.sender);
-        receiver.transfer(MINT_PRICE);
-    }
+    return result;
+  }
 }
